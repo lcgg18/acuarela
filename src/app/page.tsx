9989784bc8186c3e901d8b0aba59143589122ad0
luis <lcgg18@gmail.com>
2024-09-04 19:18:33 -0500

    o1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,8 @@ import styles from "../../styles/Home.module.css";
 export default function Home() {
   return (
     <main>
-      <h1 className={styles.title}>Bienvenidos a la APP del Conjunto</h1>
-      <h1 className={styles.subtitle}>Gracias Por Permitirnos Dirigir</h1>
-      <h1 className={styles.subtitle}>Tan Prestigiosa Copropiedad</h1>
-      <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+      <h1 className={styles.title}>Gracias Por Permitirnos Dirigir</h1>
+      <h1 className={styles.title}>Tan Prestigiosa Copropiedad</h1>
     </main>
   );
 }
